2b6642dbcb5d6819c01524a38c61a62841c1d318
pointbazaar <alex23667@gmail.com>
2019-07-06 13:01:39 +0200

    remove unused imports

--- Androidproject/app/src/main/java/com/future/lobajo/NewsFeedActivity.java
+++ Androidproject/app/src/main/java/com/future/lobajo/NewsFeedActivity.java
@@ -2,13 +2,7 @@ package com.future.lobajo;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class NewsFeedActivity extends AppCompatActivity {
 
