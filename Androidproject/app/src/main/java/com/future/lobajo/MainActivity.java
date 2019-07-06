b4cec97589e375c576d8c0b84e22b949b4e48395
pointbazaar <alex23667@gmail.com>
2019-07-06 14:13:03 +0200

    minor fix

--- Androidproject/app/src/main/java/com/future/lobajo/MainActivity.java
+++ Androidproject/app/src/main/java/com/future/lobajo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.future.lobajo;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -13,19 +15,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+
+
+        //set to maps view
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View maps_view = vi.inflate(R.layout.activity_maps, linearLayout);
+
+
+        linearLayout.addView(maps_view,0);
+
+        //container = (ViewGroup) findViewById(R.id.layoutInserPoint);
+        //container.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+
+    public void onNewsClick(View view){
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+
+
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View news_view = vi.inflate(R.layout.activity_news_feed, linearLayout);
+
+
+
+
+        //remove current shown view
+        linearLayout.removeViewAt(0);
+
+        //add view
+        linearLayout.addView(news_view,0);
+
     }
 
     public void onMapsClick(View view) {
 
 
 
+        //set to maps view
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View maps_view = vi.inflate(R.layout.activity_maps, null);
+
         LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
 
+        //remove current shown view
+        linearLayout.removeViewAt(0);
 
-        //View mapFragment = findViewById(R.id.map);
-
-        //linearLayout.addView(mapFragment,0);
+        //add view
+        linearLayout.addView(maps_view,0);
     }
 
 }
