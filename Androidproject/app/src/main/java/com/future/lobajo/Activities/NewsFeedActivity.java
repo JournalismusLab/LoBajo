e91f53718688919ea653665198b96cdc34a8f1de
LanPodder <alan.i.shref@gmail.com>
2019-07-06 17:24:52 +0200

    switch activities

--- Androidproject/app/src/main/java/com/future/lobajo/Activities/NewsFeedActivity.java
+++ Androidproject/app/src/main/java/com/future/lobajo/Activities/NewsFeedActivity.java
@@ -1,18 +1,45 @@
 package com.future.lobajo.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.future.lobajo.MainBottomNavigation;
 import com.future.lobajo.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NewsFeedActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    Intent intent = new Intent(NewsFeedActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_notifications:
+                    Intent intent3 = new Intent(NewsFeedActivity.this, ARActivity.class);
+                    startActivity(intent3);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed);
 
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
