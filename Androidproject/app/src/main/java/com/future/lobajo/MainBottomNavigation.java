2cd223a0ebcf490df802960f897415688311f015
LanPodder <alan.i.shref@gmail.com>
2019-07-06 14:08:45 +0200

    added buttomnavigationbar and a empty main activity corresponding

---
+++ Androidproject/app/src/main/java/com/future/lobajo/MainBottomNavigation.java
@@ -0,0 +1,50 @@
+package com.future.lobajo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.future.lobajo.Activities.MapsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainBottomNavigation extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_dashboard:
+                    Intent intent = new Intent(MainBottomNavigation.this, MapsActivity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bottom_navigation);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+}
