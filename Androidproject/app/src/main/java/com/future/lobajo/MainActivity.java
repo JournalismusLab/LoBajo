2abf2bda75169f56215ad928adc2832977162bb4
pointbazaar <alex23667@gmail.com>
2019-07-06 13:50:04 +0200

    Small changes

--- Androidproject/app/src/main/java/com/future/lobajo/MainActivity.java
+++ Androidproject/app/src/main/java/com/future/lobajo/MainActivity.java
@@ -2,21 +2,16 @@ package com.future.lobajo;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.future.lobajo.Activities.MapsActivity;
-import com.future.lobajo.R;
-import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_news_feed);
+        setContentView(R.layout.activity_main);
 
     }
 
