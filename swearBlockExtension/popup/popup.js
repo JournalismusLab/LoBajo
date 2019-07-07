ce5a216a75a851452027e65d419f3723e9e785af
pointbazaar <alex23667@gmail.com>
2019-07-07 04:58:31 +0200

    show and hide the buttons depending on the setting

--- swearBlockExtension/popup/popup.js
+++ swearBlockExtension/popup/popup.js
@@ -2,12 +2,28 @@
 //localStorage doesnt work here
 //localStorage.setItem("popup","true");
 
-//TODO: hide the button which is not needed based on the user settings
+//hide the button which is not needed based on the user settings
+
+var gettingItem = browser.storage.local.get("enabled");
+gettingItem.then(onGot, function(error){});
+
+function onGot(item) {
+  
+  if(item.enabled){
+	document.getElementById("button_disable").style.display="";
+	document.getElementById("button_enable").style.display="none";
+  }else{
+	document.getElementById("button_disable").style.display="none";
+	document.getElementById("button_enable").style.display="";  
+  }
+}
+
 
 document.getElementById("button_disable").addEventListener("click", function(e) {
 
   	function onExecuted(result) {
 	  console.log(`We executed in all subframes`);
+	  //location.reload();
 	}
 
 	function onError(error) {
@@ -25,6 +41,7 @@ document.getElementById("button_enable").addEventListener("click", function(e) {
 
   	function onExecuted(result) {
 	  console.log(`We executed in all subframes`);
+  	  //location.reload();
 	}
 
 	function onError(error) {
@@ -39,5 +56,7 @@ document.getElementById("button_enable").addEventListener("click", function(e) {
 	
 	
 });
+
+
 
 //console.log("popup");
